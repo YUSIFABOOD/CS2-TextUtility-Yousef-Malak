06cb2e151faf6fae00083d4f253ed945a542e133
Malak <malakelzwawy@aucegypt.edu>
2025-06-25 17:31:23 +0300

    My fnctions are declared

---
+++ text_utility.h
@@ -0,0 +1,10 @@
+#include<iostream>
+
+bool isPalindrom (char* s);
+void Uppercase (char* s);
+void Lowercase (char* s);
+int substringoccur (char* s);
+int countCharacters(const char* str);
+int countVowels(const char* str);
+int countConsonants(const char* str);
+void reverseString(char* str);
