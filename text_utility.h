68fa7e3dde8d760978f8c27d7f078957c8df24ef
YUSIFABOOD <104925454+YUSIFABOOD@users.noreply.github.com>
2025-06-26 20:01:05 +0300

    header file updated

--- text_utility.h
+++ text_utility.h
@@ -1,6 +1,11 @@
 #include<iostream>
 
 bool isPalindrom (const char* s);
-void Lowercase (char* s);
 void Uppercase (char* s);
-int substringoccur(const char* s, const char* f);
+void Lowercase (char* s);
+int substringoccur (const char* s, const char* f);
+
+int countCharacters(const char* str);
+void countVowelsAndConsonants(const char* str);
+void reverseString(char* str);
+int countW(const char* str);
