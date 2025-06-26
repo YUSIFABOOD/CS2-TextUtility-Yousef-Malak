2a25d6f0a8482a0175e93530fa5515e368a0079c
malakelzwawy509 <malakelzwawy@aucegypt.edu>
2025-06-26 17:00:26 +0300

    Update text_utility.h

--- text_utility.h
+++ text_utility.h
@@ -4,10 +4,8 @@ bool isPalindrom (char* s);
 void Uppercase (char* s);
 void Lowercase (char* s);
 int substringoccur (char* s);
-<<<<<<< HEAD
-=======
+
 int countCharacters(const char* str);
 void countVowelsAndConsonants(const char* str, int& countV, int& countC)
 void reverseString(char* str);
 int countW(const char* str)
->>>>>>>
