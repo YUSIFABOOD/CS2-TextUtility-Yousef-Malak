2ae2eec06a79e88307e00cb27587b4cd2d360b5b
YUSIFABOOD <yousefabood84@gmail.com>
2025-06-26 19:50:37 +0300

    minor changes

--- text_utility.h
+++ text_utility.h
@@ -3,9 +3,9 @@
 bool isPalindrom (const char* s);
 void Uppercase (char* s);
 void Lowercase (char* s);
-int substringoccur (const char* s);
+int substringoccur (const char* s, const char* f);
 
 int countCharacters(const char* str);
-void countVowelsAndConsonants(const char* str, int& countV, int& countC)
+void countVowelsAndConsonants(const char* str);
 void reverseString(char* str);
-int countW(const char* str)
+int countW(const char* str);
