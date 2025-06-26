9d9129314d72f34bf3bf9ab0132acfc2a67ae415
YUSIFABOOD <104925454+YUSIFABOOD@users.noreply.github.com>
2025-06-26 19:55:10 +0300

    undo

--- Lowercase.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include"text_utility.h"
-using namespace std;
-void Lowercase (char* s)
-{
-    int length =countCharacters(s);   
-    for (int i=0; i<length;i++) if (s[i]>='A'&& s[i]<='Z') s[i]+=32;
-}
