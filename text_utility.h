07f17254711f8d9e26092fd5d3d7f4d30092ddff
Malak <malakelzwawy@aucegypt.edu>
2025-06-26 18:55:29 +0300

    changes

--- text_utility.h
+++ text_utility.h
@@ -8,4 +8,4 @@ int substringoccur (const char* s);
 int countCharacters(const char* str);
 void countVowelsAndConsonants(const char* str, int& countV, int& countC)
 void reverseString(char* str);
-int countW(const char* str)
+int countW(const char* str)
